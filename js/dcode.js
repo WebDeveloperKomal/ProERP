047219244ac8490e19281e525fdf0ea8c7ede02f
KomalDeveloper <ibginfotechPvtLtd@gmail.com>
2024-11-09 14:57:42 +0530

    09/11

--- js/dcode.js
+++ js/dcode.js
@@ -13,14 +13,14 @@
     /**---------------------------------------------*
      //*------------- WOW Animation 
     **---------------------------------------------**/
-    var wow = new WOW({
-        boxClass: 'wow', //*------ default
-        animateClass: 'animated', //*------ default
-        offset: 0, //*------- default
-        mobile: true, //*------ default
-        live: true //*------ default
-    });
-    wow.init();
+    // var wow = new WOW({
+    //     boxClass: 'wow', //*------ default
+    //     animateClass: 'animated', //*------ default
+    //     offset: 0, //*------- default
+    //     mobile: true, //*------ default
+    //     live: true //*------ default
+    // });
+    // wow.init();
     /**---------------------------------------------*
      //*------------- Theme Option
     **---------------------------------------------*/
